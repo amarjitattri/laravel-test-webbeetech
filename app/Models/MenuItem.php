44b33d55ee3ea3e3cd304637b984f5f460e1c9e0
Amar Jit Attri <amarjit.attri@netsolutions.com>
2021-05-22 15:42:41 +0530

    Task 4 - Complete the getMenuItems function

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -3,10 +3,30 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+    public function scopeGetMenuItems($query)
+    {
+        $menuItems = $query->get();
+        return $this->buildMenuItems($menuItems->toArray());
+    }
 
+    public function buildMenuItems(array $elements, $parentId = 0)
+    {
+        $branch = array();
+        
+        foreach ($elements as $element) {
+            if ($element['parent_id'] == $parentId) {
+                $children = $this->buildMenuItems($elements, $element['id']);
+                if ($children) {
+                    $element['children'] = $children;
+                }
+                $branch[] = $element;
+            }
+        }
+    
+        return $branch;
+    }
 }
